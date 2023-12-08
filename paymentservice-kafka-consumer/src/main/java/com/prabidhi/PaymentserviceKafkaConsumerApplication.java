2c54499ba926fb6a44d6f19e40ed16b4a98f811c
prabidhipokhrel <test5523540@gmail.com>
2023-12-08 18:23:32 -0500

    creating packages for consumer git status

---
+++ paymentservice-kafka-consumer/src/main/java/com/prabidhi/PaymentserviceKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.prabidhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentserviceKafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentserviceKafkaConsumerApplication.class, args);
+    }
+
+}
---
+++ paymentservice-kafka-consumer/src/test/java/com/prabidhi/PaymentserviceKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabidhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentserviceKafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
