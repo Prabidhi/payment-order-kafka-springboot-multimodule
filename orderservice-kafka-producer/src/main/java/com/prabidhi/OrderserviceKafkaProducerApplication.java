417403c1a4fa0d7986131c76318fd49585bbee3e
prabidhipokhrel <test5523540@gmail.com>
2023-12-08 17:55:21 -0500

    creating packages for producer and consumer

---
+++ orderservice-kafka-producer/src/main/java/com/prabidhi/OrderserviceKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.prabidhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderserviceKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderserviceKafkaProducerApplication.class, args);
+    }
+
+}
---
+++ orderservice-kafka-producer/src/test/java/com/prabidhi/OrderserviceKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabidhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceKafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
